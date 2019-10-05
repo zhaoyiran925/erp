f59bc428c47fd459363a829a2f25e950924d61b4
mldn <mldnqa@163.com>
2019-10-05 09:17:27 +0800

    Commit zan_cun directory file

--- Hello.java
+++ Hello.java
@@ -5,5 +5,7 @@ public class Hello
 	 // System.out.println("认真听课的孩子才是好孩子");
 	 System.out.println("Hello Git!!");
 	 System.out.println("Hello Git!!");
+	 System.out.println("世界你好");
+	 System.out.println(new java.util.Date());
 		 
 }
